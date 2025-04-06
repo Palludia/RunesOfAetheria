99dacc4861dac57da4df656f20a506d9e34631de
Kiryl0047 <kiryl0047@gmail.com>
2025-04-06 15:21:37 +0800

    Monsters Colliding With Player

---
+++ res/maps/gentle forest (48x48 resize) v01.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="gentle forest (48x48 resize) v01" tilewidth="48" tileheight="48" tilecount="256" columns="16">
+ <image source="gentle forest (48x48 resize) v01.png" trans="ff00ff" width="768" height="768"/>
+ <tile id="80">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="14.9267" y="0" width="33.0733" height="48"/>
+  </objectgroup>
+ </tile>
+ <tile id="83">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="30.1011" height="48"/>
+  </objectgroup>
+ </tile>
+ <tile id="96">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="15" y="0" width="33.0733" height="48"/>
+  </objectgroup>
+ </tile>
+ <tile id="99">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="30.1011" height="48"/>
+  </objectgroup>
+ </tile>
+</tileset>
